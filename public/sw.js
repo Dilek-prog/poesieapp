b3dd05b94f135672625a0b2a8e424c3d9662ef75
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2022-08-11 18:33:48 +0200

    servis Worker- dynamik, statistic

--- public/sw.js
+++ public/sw.js
@@ -7,8 +7,22 @@ workbox.routing.registerRoute(
     new workbox.strategies.NetworkFirst()     // NetworkFirst() vs CacheFirst()
 )
 
+const CURRENT_STATIC_CACHE = 'static-v2';
+const CURRENT_DYNAMIC_CACHE = 'dynamic-v2';
+
 self.addEventListener('activate', event => {
     console.log('service worker --> activating ...', event);
+    event.waitUntil(
+        caches.keys()
+            .then( keyList => {
+                return Promise.all(keyList.map( key => {
+                    if(key !== 'static-v1' && key !== 'dynamic') {
+                        console.log('service worker --> old cache removed :', key);
+                        return caches.delete(key);
+                    }
+                }))
+            })
+    );
     return self.clients.claim();
 })
 
@@ -20,10 +34,43 @@ self.addEventListener('fetch', event => {
 self.addEventListener('install', event => {
     console.log('service worker --> installing ...', event);
     event.waitUntil(
-        caches.open('static')
+        caches.open(CURRENT_STATIC_CACHE)
             .then( cache => {
-                console.log('Service-Worker-Cache erzeugt und offen');
-                cache.add('/src/js/app.js');
+                console.log('Service-Worker-Cache erzeugt und offen'); 
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/htw.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css'
+                ]);
+            })
+    );
+})
+
+self.addEventListener('fetch', event => {
+    if (!(event.request.url.indexOf('http') === 0)) return;
+    event.respondWith(
+        caches.match(event.request)
+            .then( response => {
+                if(response) {
+                    return response;
+                } else {
+                    return fetch(event.request)
+                        .then( res => {     // nicht erneut response nehmen, haben wir schon
+                            return caches.open('dynamic')      // neuer, weiterer Cache namens dynamic
+                                .then( cache => {
+                                    cache.put(event.request.url, res.clone());
+                                    return res;
+                                })
+                        });
+                }
             })
     );
 })
