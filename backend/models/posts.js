8d58c767b52c2dd8ca34f1a4a3275211569e502e
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2022-08-12 11:07:02 +0200

    add Post schema

---
+++ backend/models/posts.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+    title: String,
+    location: String,
+    image_id: String,
+    text: String
+});
+
+module.exports = mongoose.model('Post', schema);
