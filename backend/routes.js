80f0e4e8ce3efa989dc98f724092e3d996d6f9a5
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2022-08-12 11:23:29 +0200

    add get & Post request

--- backend/routes.js
+++ backend/routes.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('./models/posts');
 
-// eine GET-Anfrage
-router.get('/', async(req, res) => {
+// GET all posts
+router.get('/posts', async(req, res) => {
+    const allPosts = await Post.find();
+    console.log(allPosts);
+    res.send(allPosts);
+});
 
-    res.send({ message: "Hello Poesie!" });
+// POST one post
+router.post('/posts', async(req, res) => {
+    const newPost = new Post({
+        title: req.body.title,
+        location: req.body.location,
+        image_id: req.body.image_id,
+        text: req.body.text
+    })
+    await newPost.save();
+    res.send(newPost);
 });
 
 module.exports = router;
