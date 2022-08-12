52b03354b796ec2f5738e62bb1591d371f2ac065
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2022-08-12 11:38:20 +0200

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
@@ -7,6 +8,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+app.use(cors());
 app.use('/', routes);
 
 app.listen(PORT, (error) => {
