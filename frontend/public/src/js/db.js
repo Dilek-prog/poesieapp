8f1adc9d28668b1ea911e9e7e944d96afefcc140
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2022-08-13 14:50:24 +0200

    add db.js (transkription)

---
+++ frontend/public/src/js/db.js
@@ -0,0 +1,23 @@
+const db = idb.openDB('posts-store', 1, {
+    upgrade(db) {
+        // Create a store of objects
+        const store = db.createObjectStore('posts', {
+            // The 'id' property of the object will be the key.
+            keyPath: 'id',
+            // If it isn't explicitly set, create a value by auto incrementing.
+            autoIncrement: true,
+        });
+        // Create an index on the 'id' property of the objects.
+        store.createIndex('id', 'id');
+    },
+});
+
+function writeData(st, data) {
+    return db
+        .then( dbPosts => {
+            let tx = dbPosts.transaction(st, 'readwrite');
+            let store = tx.objectStore(st);
+            store.put(data);
+            return tx.done;
+        })
+}
