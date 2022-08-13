29d5d9fbfb42fb8a442e359e8120688f7312bdd6
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2022-08-13 13:40:18 +0200

    static Files in Array umgewandelt

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -30,33 +30,45 @@ self.addEventListener('fetch', event => {
     event.respondWith(fetch(event.request));
 })
 
+const STATIC_FILES = [
+    '/',
+    '/index.html',
+    '/src/js/app.js',
+    '/src/js/feed.js',
+    '/src/js/material.min.js',
+    '/src/js/idb.js',
+    '/src/css/app.css',
+    '/src/css/feed.css',
+    '/src/images/htw.jpg',
+    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css'
+];
+
 self.addEventListener('install', event => {
     console.log('service worker --> installing ...', event);
     event.waitUntil(
         caches.open(CURRENT_STATIC_CACHE)
-            .then( cache => {
-                console.log('Service-Worker-Cache erzeugt und offen'); 
-                cache.addAll([
-                    '/',
-                    '/index.html',
-                    '/src/js/app.js',
-                    '/src/js/feed.js',
-                    '/src/js/idb.js',
-                    '/src/js/material.min.js',
-                    '/src/css/app.css',
-                    '/src/css/feed.css',
-                    '/src/images/htw.jpg',
-                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
-                    'https://fonts.googleapis.com/icon?family=Material+Icons',
-                    'https://code.getmdl.io/1.3.0/material.blue_grey-red.min.css'
-                ]);
-            })
+        .then(cache => {
+            console.log('Service-Worker-Cache erzeugt und offen');
+            cache.addAll(STATIC_FILES);
+        })
     );
 })
 
-self.addEventListener('fetch', event => {
-    if (!(event.request.url.indexOf('http') !== 0)) return;
 
+self.addEventListener('fetch', event => {
+    if (!(event.request.url.indexOf('http') === 0)) return;
+    const url = 'http://localhost:3000/posts';
+    if(event.request.url.indexOf(url) >= 0) {
+        event.respondWith(
+            fetch(event.request)
+                .then ( res => {
+                    // hier Anfrage an http://localhost:3000/posts behandeln
+                    return res;
+                })
+        )
+    } else {
     event.respondWith(
         caches.match(event.request)
             .then( response => {
@@ -73,5 +85,5 @@ self.addEventListener('fetch', event => {
                         });
                 }
             })
-    );
+    )}
 })
