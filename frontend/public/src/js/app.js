3f50b06cf4e3a75724a7dba08fa8c2c89e43fa8c
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2022-08-21 08:37:06 +0200

    Notification on

--- frontend/public/src/js/app.js
+++ frontend/public/src/js/app.js
@@ -1,3 +1,5 @@
+let enableNotificationsButtons = document.querySelectorAll('.enable-notifications'); //Variable, die auf ein Array aller Button mit der CSS klasse .. zeigt
+
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker
         .register('/sw.js')
@@ -7,4 +9,23 @@ if ('serviceWorker' in navigator) {
         .catch(
             err => { console.log(err); }
         );
+}
+
+function askForNotificationPermission() { //Klickereignis, wenn der Browser die API unterstützt, wird sie verwendet 
+    Notification.requestPermission( result => {
+        console.log('User choice', result);
+        if(result !== 'granted') {
+            console.log('No notification permission granted');
+        } else {
+            // displayConfirmNotification();
+            configurePushSubscription();
+        }
+    });
+}
+
+if('Notification' in window && 'serviceWorker' in navigator) { //prüft, ob der Browser Notification API unterstützt
+    for(let button of enableNotificationsButtons) {
+        button.style.display = 'inline-block';
+        button.addEventListener('click', askForNotificationPermission);
+    }
 }
