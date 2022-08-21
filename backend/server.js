1725c11d9e36701b2f1241d0e9caf55afbf18701
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2022-08-21 09:22:58 +0200

    Endpunkt in Backend implementieren from subscriptionRoute

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const cors = require('cors');
 const postsRoutes = require('./routes/posts.routes');
+const uploadRoutes = require('./routes/upload.routes');
+const downloadRoute = require('./routes/download.routes');
+const deleteRoute = require('./routes/delete.routes');
+const subscriptionRoute = require('./routes/subscription.routes');
+
 const mongoose = require('mongoose');
+
 require('dotenv').config();
 
 const app = express();
@@ -10,6 +16,10 @@ const PORT = 3000;
 app.use(express.json());
 app.use(cors());
 app.use('/posts', postsRoutes);
+app.use('/image', uploadRoutes);
+app.use('/download', downloadRoute);
+app.use('/delete', deleteRoute);
+app.use('/subscription', subscriptionRoute);
 
 
 
