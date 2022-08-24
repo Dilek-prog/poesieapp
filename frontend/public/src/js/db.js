33c93ac0c459528956d8739da482c75451ac7dfe
Dilek-prog <82120659+Dilek-prog@users.noreply.github.com>
2022-08-24 16:00:56 +0200

    store db

--- frontend/public/src/js/db.js
+++ frontend/public/src/js/db.js
@@ -1,31 +1,33 @@
-const db = idb.openDB('posts-store', 1, {
+
+const db = idb.openDB('posts-store', 2, {
     upgrade(db) {
         // Create a store of objects
         const store1 = db.createObjectStore('posts', {
-            // The 'id' property of the object will be the key.
+            // The '_id' property of the object will be the key.
             keyPath: '_id',
             // If it isn't explicitly set, create a value by auto incrementing.
             autoIncrement: true,
         });
-        // Create an index on the 'id' property of the objects.
+        // Create an index on the '_id' property of the objects.
         store1.createIndex('_id', '_id');
-
+        
+        // Create another store of objects
         const store2 = db.createObjectStore('sync-posts', {
             keyPath: 'id',
+            autoIncrement: true,
         });
         store2.createIndex('id', 'id');
     },
-});
-
+})
 
 function writeData(st, data) {
     return db
-        .then( dbPosts => {
-            let tx = dbPosts.transaction(st, 'readwrite');
-            let store = tx.objectStore(st);
-            store.put(data);
-            return tx.done;
-        })
+    .then( dbPosts => {
+        let tx = dbPosts.transaction(st, 'readwrite');
+        let store = tx.objectStore(st);
+        store.put(data);
+        return tx.done;
+    })
 }
 
 function readAllData(st) {
@@ -36,6 +38,7 @@ function readAllData(st) {
             return store.getAll();
         })
 }
+
 function clearAllData(st) {
     return db
         .then( dbPosts => {
@@ -44,4 +47,17 @@ function clearAllData(st) {
             store.clear();
             return tx.done;
         })
+}
+
+function deleteOneData(st, _id) {
+    db
+    .then( dbPosts => {
+        let tx = dbPosts.transaction(st, 'readwrite');
+        let store = tx.objectStore(st);
+        store.delete(_id);
+        return tx.done;
+    })
+    .then( () => {
+        console.log('Data deleted ...');
+    });
 }
